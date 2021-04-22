0eefa49be3466feb497f0a25d7877a7da4e64c70
Light <47911535+LightAPIs@users.noreply.github.com>
2021-04-22 17:04:25 +0800

    fix(unread): 修复在点击通知打开网址时会使未读计数减 2 的问题

--- src/enhanced-notification/Notification.ts
+++ src/enhanced-notification/Notification.ts
@@ -420,7 +420,7 @@ abstract class Notification<T extends enhanced.NotificationOptions> implements E
                   }
                 );
               }
-              !this.options.isTest && store.commit('decreaseUnread');
+
               if (!this.options.isTest) {
                 store.commit('decreaseUnread');
                 store.commit('visitNotification', this.options.id);
